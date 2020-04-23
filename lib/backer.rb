c6971293ff34bfe2758fe1f15f44ab2a0785af38
aurel fontaine <ajfame11@gmail.com>
2020-04-23 02:25:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,10 @@
 class Backer
   attr_reader :backed_projects
   
-  def initialize
+  def initialize(backed_projects)
     @back_projects = []
   end
+  
+  def backed_project(project)
+  @backed_projects << project
 end
